e9791a74ae756b0fed83e04fad6d9c02f6df7070
davemc85 <davemcallister@hotmail.co.uk>
2019-06-07 18:49:53 +0100

    drumkit class and test added

--- src/main/java/instruments/DrumKit.java
+++ src/main/java/instruments/DrumKit.java
@@ -1,4 +1,19 @@
 package instruments;
 
-public class DrumKit {
+public class DrumKit extends Instrument {
+
+    private int numberOfDrums;
+
+    public DrumKit(String name, double sellingPrice, double purchasedPrice, Family family, String material, int numberOfDrums) {
+        super(name, sellingPrice, purchasedPrice, family, material);
+        this.numberOfDrums = numberOfDrums;
+    }
+
+    public int getNumberOfDrums() {
+        return numberOfDrums;
+    }
+
+    public void setNumberOfDrums(int numberOfDrums) {
+        this.numberOfDrums = numberOfDrums;
+    }
 }
---
+++ src/test/java/instrumentTests/DrumKitTest.java
@@ -0,0 +1,84 @@
+package instrumentTests;
+
+import instruments.DrumKit;
+import instruments.Family;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DrumKitTest {
+
+    private DrumKit drumKit;
+
+    @Before
+    public void setUp(){
+        drumKit = new DrumKit("Pearl", 1500, 850, Family.PERCUSSION, "Mahogany", 5 );
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Pearl", drumKit.getName());
+    }
+
+    @Test
+    public void canSetName(){
+        drumKit.setName("Ludwig");
+        assertEquals("Ludwig", drumKit.getName());
+    }
+
+    @Test
+    public void canGetSellingPrice(){
+        assertEquals(1500, drumKit.getSellingPrice(), 0.00);
+    }
+
+    @Test
+    public void canSetSellingPrice(){
+        drumKit.setSellingPrice(2000);
+        assertEquals(2000, drumKit.getSellingPrice(), 0.00);
+    }
+
+    @Test
+    public void canGetPurchasedPrice(){
+        assertEquals(850, drumKit.getPurchasedPrice(), 0.00);
+    }
+
+    @Test
+    public void canSetPurchasedPrice(){
+        drumKit.setPurchasedPrice(350);
+        assertEquals(350, drumKit.getPurchasedPrice(), 0.00);
+    }
+
+    @Test
+    public void canGetInstrumentFamily(){
+        assertEquals(Family.PERCUSSION, drumKit.getFamily());
+    }
+
+    @Test
+    public void canSetInstrumentFamily(){
+        drumKit.setFamily(Family.BRASS);
+        assertEquals(Family.BRASS, drumKit.getFamily());
+    }
+
+    @Test
+    public void canGetMaterial(){
+        assertEquals("Mahogany", drumKit.getMaterial());
+    }
+
+    @Test
+    public void canSetMaterial(){
+        drumKit.setMaterial("Beech");
+        assertEquals("Beech", drumKit.getMaterial());
+    }
+
+    @Test
+    public void canGetNumberOfDrums(){
+        assertEquals(5, drumKit.getNumberOfDrums());
+    }
+
+    @Test
+    public void canSetNumberOfDrums(){
+        drumKit.setNumberOfDrums(4);
+        assertEquals(4, drumKit.getNumberOfDrums());
+    }
+}
