bf9b8250f22017c20dd2cceaac610459a6bb470e
davemc85 <davemcallister@hotmail.co.uk>
2019-06-08 12:51:04 +0100

    sheet music class added

---
+++ src/main/java/accessories/SheetMusic.java
@@ -0,0 +1,8 @@
+package accessories;
+
+public class SheetMusic extends Accessory {
+
+    public SheetMusic(String name, double sellingPrice, double purchasedPrice) {
+        super(name, sellingPrice, purchasedPrice);
+    }
+}
---
+++ src/test/java/accessoriesTest/SheetMusicTest.java
@@ -0,0 +1,56 @@
+package accessoriesTest;
+
+import accessories.DrumSticks;
+import accessories.SheetMusic;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SheetMusicTest {
+
+    private SheetMusic sheetMusic;
+
+    @Before
+    public void setUp(){
+        sheetMusic = new SheetMusic("100 Great Songs", 20, 7.50);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("100 Great Songs", sheetMusic.getName());
+    }
+
+    @Test
+    public void canSetName(){
+        sheetMusic.setName("Classical Hits");
+        assertEquals("Classical Hits", sheetMusic.getName());
+    }
+
+    @Test
+    public void canGetSellingPrice(){
+        assertEquals(20, sheetMusic.getSellingPrice(), 0.00);
+    }
+
+    @Test
+    public void canSetSellingPrice(){
+        sheetMusic.setSellingPrice(15);
+        assertEquals(15, sheetMusic.getSellingPrice(), 0.00);
+    }
+
+    @Test
+    public void canGetPurchasedPrice(){
+        assertEquals(7.50, sheetMusic.getPurchasedPrice(), 0.00);
+    }
+
+    @Test
+    public void canSetPurchasedPrice(){
+        sheetMusic.setPurchasedPrice(10);
+        assertEquals(10, sheetMusic.getPurchasedPrice(), 0.00);
+    }
+
+    @Test
+    public void canGetCalculateMarkup(){
+        assertEquals(12.50, sheetMusic.calculateMarkup(), 0.00);
+    }
+}
